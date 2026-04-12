b94b6593c85b2bb56fdc0ef255454800d1734f4a
CodeMan62 <sharmahimanshu15082007@gmail.com>
2026-04-13 00:28:02 +0530

    MLA(still haven't filled model config values and i think some quick code changes may be needed in MLA class)

--- inference/vmoe.py
+++ inference/vmoe.py
@@ -2,6 +2,77 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
+
+#--------------------------------------------------------------------------------
+
+class ModelConfig:
+    d_model: int = 128
+    d_c: int = ...
+    n_heads: int = 4
+    d_comp: int = ...
+    max_seq_len: int = ...
+    d_hr: int = ...
+    d_rope: int = ...
+
+
+class RmsNorm(nn.Module):
+    def __init__(self, config: ModelConfig, eps=1e-5):
+        super().__init__()
+        self.d_model = config.d_model
+        self.g = nn.Parameter(torch.ones(self.d_model))
+        self.eps = eps
+    def forward(self, x:torch.Tensor):
+        rms = torch.sqrt(x.pow(2).mean(dim=-1, keepdim=True) + self.eps)
+        return x / rms * self.g
+
+class MLA(nn.Module):
+    def __init__(self, config: ModelConfig):
+        self.dim = config.d_model
+        self.num_heads = config.n_heads
+        self.d_heads = self.dim // self.num_heads
+        self.dc_comp = config.d_comp
+        self.d_c = config.d_c
+        # down and up proj matrix for k and v
+        self.down_proj = nn.Linear(self.dim, self.d_c)
+        self.up_proj = nn.Linear(self.d_c, self.d_heads*self.num_heads)
+        # down and up proj matrix for q
+        self.qdown_proj = nn.Linear(self.dim, self.dc_comp)
+        self.qup_proj = nn.Linear(self.dc_comp, self.d_heads*self.num_heads)
+        self.w_qr = nn.Linear(self.dc_comp, self.num_heads*config.d_rope)
+        self.w_kr = nn.Linear(self.dim, self.num_heads*config.d_rope)
+        self.output = nn.Linear(self.num_heads*self.d_heads, self.dim)
+    def forward(self, x: torch.Tensor):
+        # compressed latent vecotr of keys and values
+        batch_size, seq_len, _ = x.shape
+        kv_compressed = self.down_proj(x)
+        k_compressed = self.up_proj(kv_compressed)
+        v_compressed = self.up_proj(kv_compressed)
+        q_compressed = self.qdown_proj(x)
+        q_c = self.qup_proj(q_compressed)
+        q_r = RoPE(self.w_qr(q_compressed))
+        k_r = RoPE(self.w_kr(x))
+        q = torch.cat([q_c, q_r], dim=-1)
+        k = torch.cat([k_compressed, k_r], dim=-1)
+        scores = torch.einsum("bqhd, bkhd->bhqk", q, k) / math.sqrt(self.d_heads)
+        score_soft = F.softmax(scores, dim=-1)
+        out = torch.einsum("bhqk, bkhd->bqhd", score_soft, v_compressed)
+        u = self.output(out.contiguous().view(batch_size, seq_len, -1)), (kv_compressed, k_r)
+
+
+
+class RoPE(nn.Module):
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+        self.dim = config.d_model
+        assert config.dim % 2 == 0
+        self.seq_len = config.max_seq_len
+    def forward(self, x: torch.Tensor, pos: torch.Tensor) -> torch.Tensor:
+        x = torch.view_as_complex(x.float().view(*x.shape[:-1], -1, 2))
+        pos = pos.view(1, x.size(1), 1, x.size(-1))
+        y = torch.view_as_real(x * pos).flatten(3)
+        return y
+
+
 class Expert(nn.Module):
     def __init__(
         self,
@@ -68,5 +139,3 @@ class MoE(nn.Module):
             idx, top = torch.where([indices == i])
             y[idx] += expert(x[idx]) * weights[idx, top, None]
         return y.view(shape)
-
-
